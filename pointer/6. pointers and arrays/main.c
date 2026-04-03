44f0efe37dbb7c403cc779d559cb1844aef0ba3f
BestFansi <baigeisnfansi@outlook.com>
2026-04-04 08:37:50 +1000

    upload

--- pointer/6. pointers and arrays/main.c
+++ pointer/6. pointers and arrays/main.c
@@ -2,5 +2,21 @@
 #include <stdio.h>
 
 int main() {
-    
+    int A[] = {2, 4, 5, 8, 1}; //5 * 4 = 20 bytes
+    int *p = A;
+    /*
+    A++; //invalid
+    p++; //valid
+    */
+    printf("%d\n", A);
+    printf("%d\n", *A);
+    printf("%d\n", A + 1);
+    printf("%d\n", *(A + 1));
+    /*
+    element at index i --
+        Address - &A[i] or (A+i)
+        Value - A[i] or *(A+i)
+        A - gives base address
+    */
+    return 0;
 }
